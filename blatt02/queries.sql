02e99938ba8b97b2d067c09279bfeb0c7eb29ca5
samydafir <samydafir@yahoo.de>
2016-10-20 11:52:53 +0200

    updated queries

--- blatt02/queries.sql
+++ blatt02/queries.sql
@@ -1,13 +1,13 @@
 \timing
-SELECT ssnum
-FROM Employee e1, Techdept
+SELECT count(ssnum)
+FROM Employee e1
 WHERE salary > (SELECT AVG(e2.salary)
 FROM Employee e2, Techdept
 WHERE e2.dept = e1.dept
 AND e2.dept = Techdept.dept);
 
-SELECT ssnum
+SELECT count(ssnum)
 FROM Techdept t, Employee e1, (SELECT dept, AVG(salary) as avgsalary from Employee group by dept) as e2
-WHERE e1.dept = t.dept AND e1.salary > e2.avgsalary;
+WHERE e1.dept = t.dept AND e1.salary > e2.avgsalary AND e1.dept = e2.dept;
 
 
