2613ffdfe427aa1ceb22fd38fde18faf9d309895
schoergk <kevin@schoergnhofer.de>
2016-11-17 09:18:36 +0100

    changed create_tables.sql

--- blatt04/create_tables.sql
+++ blatt04/create_tables.sql
@@ -1,10 +1,4 @@
-CREATE TABLE Auth
-(
-name varchar(49),
-pubID varchar(129),
-);
-
-CREATE TABLE Publ 
+CREATE TABLE Publ_S
 (
 pubID varchar(129),
 type varchar(13),
@@ -13,3 +7,37 @@ booktitle varchar(132),
 year varchar(4),
 publisher varchar(196)
 );
+
+CREATE TABLE Publ_B
+(
+pubID varchar(129),
+type varchar(13),
+title varchar(700),
+booktitle varchar(132),
+year varchar(4),
+publisher varchar(196)
+);
+CREATE INDEX pubIDB ON Publ_B (pubID);
+
+CREATE TABLE Publ_CB
+(
+pubID varchar(129),
+type varchar(13),
+title varchar(700),
+booktitle varchar(132),
+year varchar(4),
+publisher varchar(196)
+);
+CREATE INDEX pubIDCB ON Publ_CB (pubID);
+CLUSTER Publ_CB USING pubIDCB;
+
+CREATE TABLE Publ_H
+(
+pubID varchar(129),
+type varchar(13),
+title varchar(700),
+booktitle varchar(132),
+year varchar(4),
+publisher varchar(196)
+);
+CREATE INDEX pubIDH ON Publ_H USING HASH (pubID);
