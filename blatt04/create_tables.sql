547f9c36c9a359bc100532279511c23091c33bed
schoergk <kevin@schoergnhofer.de>
2016-11-17 09:04:08 +0100

    added file for creating the tables

---
+++ blatt04/create_tables.sql
@@ -0,0 +1,15 @@
+CREATE TABLE Auth
+(
+name varchar(49),
+pubID varchar(129),
+);
+
+CREATE TABLE Publ 
+(
+pubID varchar(129),
+type varchar(13),
+title varchar(700),
+booktitle varchar(132),
+year varchar(4),
+publisher varchar(196)
+);
