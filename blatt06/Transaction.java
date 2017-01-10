6873576950e83e59561a2603e53cfc57070f4562
Dominik Baumgartner <dbaumgart@cosy.sbg.ac.at>
2017-01-10 13:59:46 +0100

    Transaction almost working

--- blatt06/Transaction.java
+++ blatt06/Transaction.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * Dummy transaction that prints a start message, waits for a random time
@@ -15,47 +16,77 @@ class Transaction extends Thread {
 	int id;
 	int whichQuery;
 	Connection con = null;
-	BufferedReader credentials;
-	
-	
+	Statement stmt = null;
+	String query1;
+	String query2;
 
 	Transaction(int id, int isolationLevel, int whichQuery) throws SQLException, IOException, ClassNotFoundException {
 		this.id = id;
 		this.whichQuery = whichQuery;	
-		credentials = new BufferedReader(new FileReader("../credentials.txt"));
-		con = getCon();
-		//con.setAutoCommit(false);
-		//con.setTransactionIsolation(isolationLevel);
-
+		long startTime = 0;
+		long endTime = 0;
+		long runTime = 0;
 	}
 
 	@Override
 	public void run() {
-		System.out.println("transaction " + id + " started");
-
-		String query1 = "BEGIN; SET TRANSACTION ISOLATION LEVEL SERIALIZABLE; SET e = SELECT balance FROM Accounts WHERE account =" + this.id + ";"
-			  	  + "UPDATE Accounts SET balance = e + 1 WHERE account =" + this.id + ";"
-			  	  + "SET c = SELECT balance FROM Accounts WHERE account = 0;"
-			  	  + "UPDATE Accounts SET balance = c - 1 WHERE account = 0; END; COMMIT;";
-
-		String query2 = "START TRANSACTION ISOLATION LEVEL SERIALIZABLE; UPDATE Accounts set balance = balance + 1 where account = " + this.id + ";"
-	               	      + "UPDATE Accounts set balance = balance - 1 where account = 0; COMMIT;";
+		System.out.println("transaction " + this.id + " started");
+		startTime = System.currentTimeMillis();
 		try {
-			con.createStatement().execute(whichQuery == 1 ? query1 : query2);
-		} catch (SQLException e) {			
-			//e.printStackTrace();			
-			run();
-		}
-		try{
+			con = getCon();
+			con.setAutoCommit(false);
+			stmt = con.createStatement();
+
+			if(whichQuery == 1){
+				query1 = "START TRANSACTION ISOLATION LEVEL READ COMMITTED;";
+				stmt.executeUpdate(query1);
+
+				query1 = "SET e = SELECT balance FROM accounts WHERE account =" + this.id + ";";
+				stmt.executeUpdate(query1);
+
+				query1 = "UPDATE accounts SET balance = e + 1 WHERE account =" + this.id + ";";
+				stmt.executeUpdate(query1);
+
+				query1 = "SET c = SELECT balance FROM accounts WHERE account = 0;";
+				stmt.executeUpdate(query1);
+
+				query1 = "UPDATE accounts SET balance = c - 1 WHERE account = 0;";
+				stmt.executeUpdate(query1);
+
+			}else if(whichQuery == 2){
+				query2 = "START TRANSACTION ISOLATION LEVEL SERIALIZABLE;";
+				stmt.executeUpdate(query2);
+
+				query2 = "UPDATE accounts set balance = balance + 1 where account = " + this.id + ";";
+				stmt.executeUpdate(query2);
+
+				query2 = "UPDATE accounts set balance = balance - 1 where account = 0;";
+				stmt.executeUpdate(query2);
+			}
+			stmt.close();
+			con.commit();
 			con.close();
-		}catch(SQLException e){
+			endTime = System.currentTimeMillis();
+		} catch (IOException | SQLException e) {			
 			e.printStackTrace();
+			try{
+				stmt.close();
+				con.rollback();
+				con.close();
+				run();
+			}catch (SQLException f) {
+				f.printStackTrace();
+			}
 		}
 	}
 	
+	public long getRuntime(){
+		runTime = endTime - startTime;
+		
 	
 	public Connection getCon() throws SQLException, IOException {
 		
+		BufferedReader credentials = new BufferedReader(new FileReader("../credentials.txt"));
 		String host = "biber.cosy.sbg.ac.at";
 		String port = "5432";
 		String database = "dbtuning_ws2016";
